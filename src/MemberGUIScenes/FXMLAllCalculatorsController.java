464d5cadf5e58f511d792ca64f88cecc6138d747
Myo Myat Min <97108170+MyoMyatMin01@users.noreply.github.com>
2022-03-07 17:52:37 +0630

    delete unnecessary code

--- src/MemberGUIScenes/FXMLAllCalculatorsController.java
+++ src/MemberGUIScenes/FXMLAllCalculatorsController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MemberGUIScenes;
 
 import com.jfoenix.controls.JFXButton;
@@ -26,10 +21,6 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-/**
- *
- * @author User
- */
 public class FXMLAllCalculatorsController implements Initializable {
 
     @FXML
