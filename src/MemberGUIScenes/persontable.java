2084d347eada0cc84a65116858c78ade7b23f491
Myo Myat Min <97108170+MyoMyatMin01@users.noreply.github.com>
2022-03-17 22:33:25 +0630

    delete unnecessary code

--- src/MemberGUIScenes/persontable.java
+++ src/MemberGUIScenes/persontable.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MemberGUIScenes;
 
-/**
- *
- * @author User
- */
 public class persontable {
 
     String tblLife, tblPro;
@@ -34,7 +25,4 @@ public class persontable {
         this.tblPro = tblPro;
     }
     
-    
 }
-
-
